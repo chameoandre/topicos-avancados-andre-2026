bf899f0255ad47561868fb3cf566c90429bfc24a
chameoandre <chameoandre@users.noreply.github.com>
2026-04-16 15:35:41 -0300

    Atualizando visualização http externa para localhost

--- roteiro-05-flask-crud/api-alunos/app.py
+++ roteiro-05-flask-crud/api-alunos/app.py
@@ -86,7 +86,7 @@ def atualizar_aluno(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 
 # curl -X POST http://127.0.0.1:5000/alunos \
